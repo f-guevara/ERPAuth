cc1ab6d5579fffc076d8348621d69f59a2011988
fergu <fernandoguevara.sv@gmail.com>
2024-11-21 19:56:09 +0100

    Created modal.js and additional JS logic, to activate the modal in OrderManagement to delete orders

---
+++ ERPAuth.Client/wwwroot/modal.js
@@ -0,0 +1,23 @@
+﻿function openModal(modalId) {
+    var modalElement = document.getElementById(modalId);
+    if (modalElement) {
+        var modal = new bootstrap.Modal(modalElement);
+        modal.show();
+    } else {
+        console.error(`Modal with ID '${modalId}' not found.`);
+    }
+}
+
+function closeModal(modalId) {
+    var modalElement = document.getElementById(modalId);
+    if (modalElement) {
+        var modal = bootstrap.Modal.getInstance(modalElement);
+        if (modal) {
+            modal.hide();
+        } else {
+            console.error(`Bootstrap modal instance for ID '${modalId}' not found.`);
+        }
+    } else {
+        console.error(`Modal with ID '${modalId}' not found.`);
+    }
+}
